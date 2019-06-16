cc817170650d29b7a14837b988c96efc3db38c99
ymx <1738465125@qq.com>
2019-06-16 17:35:48 +0800

    页面样式调整基本完成

---
+++ js/base.js
@@ -0,0 +1,10 @@
+// window.onload = function(){
+// 		document.querySelector('.back-page').addEventListener('click', function(){
+// 		alert('hhh');
+// 		document.go(-1);
+// 	})
+// }
+
+// document.querySelector('.back-page').addEventListener('click', function(){
+// 		alert('hhh');
+// 	})
