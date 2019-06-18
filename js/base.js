9012b52525e9a40af587e4c464f96399177e1902
ymx <1738465125@qq.com>
2019-06-19 00:17:31 +0800

    界面整体完成

--- js/base.js
+++ js/base.js
@@ -1,10 +1,15 @@
-// window.onload = function(){
-// 		document.querySelector('.back-page').addEventListener('click', function(){
-// 		alert('hhh');
-// 		document.go(-1);
-// 	})
-// }
+$(function(){
+	// 控制表格详细数据的显示
+	$('.data-item-main .more').click(function(){
+		
+		$(this).parent().parent().parent().find('.data-item-other').toggleClass('active');
+		// $('.data-item-other').not($(this)).parent().parent().parent().find('.data-item-other').removeClass('active');
+	});
 
-// document.querySelector('.back-page').addEventListener('click', function(){
-// 		alert('hhh');
-// 	})
+
+	// 控制我的账户下积分信息的显示
+	$('.page-tab .item').click(function(){
+		$('.page-tab .item').removeClass('active');
+		$(this).addClass('active');
+	})
+});
