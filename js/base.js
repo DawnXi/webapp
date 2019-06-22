231b46b8acbd3d7259f3cc7acd1b6255da4fea62
ymx <1738465125@qq.com>
2019-06-23 00:09:54 +0800

    * 添加买入，预约，卖出商品弹窗，修改样式细节 （还剩下表单提交对接，数据渲染对接，分页和更多数据展示页编写） * 还有一个团队组织树没想到办法

--- js/base.js
+++ js/base.js
@@ -11,5 +11,48 @@ $(function(){
 	$('.page-tab .item').click(function(){
 		$('.page-tab .item').removeClass('active');
 		$(this).addClass('active');
+	});
+
+
+	// 控制弹出框显示
+	// 点击遮罩层/右上角关闭按钮关闭弹框
+	$('.model-container-bg,.close-model').click(function () {
+		$('.model-body').hide();
+		$('.model-container-bg').hide();
+	})
+
+	//点击查看推广链接
+	$('.top-tip.text').click(function(){
+		$('.model-container-bg').show();
+		$('#tip').show();
+	});
+
+
+	//点击查看提示弹窗
+	$('.top-tip.spread_link').click(function(){
+		$('.model-container-bg').show();
+		$('#spread-link').show();
+	});
+
+	
+
+
+	// 订单相关弹窗
+	$('.buy').click(function(){
+		$('.model-container-bg').show();
+		$('.model-body').hide();
+		$('#buy-order').show();
+	});
+
+	$('.subscribe').click(function(){
+		$('.model-container-bg').show();
+		$('.model-body').hide();
+		$('#subscribe-order').show();
+	});
+
+	$('.sell').click(function(){
+		$('.model-container-bg').show();
+		$('.model-body').hide();
+		$('#sell-order').show();	
 	})
 });
